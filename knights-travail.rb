efa10a996e81ef28d8cafb63ffbb2a13a4974eac
Brock McElroy <28877984+brockt0pus@users.noreply.github.com>
2017-11-27 20:20:48 -0700

    Integrate binary search tree.

--- knights-travail.rb
+++ knights-travail.rb
@@ -1,23 +1,30 @@
 class Knight
-  attr_reader :pos
-  def initialize(pos)
-    @pos = pos
+  require "binary-tree"
+
+  def initialize(start)
+    @start = start
     @movements = [[2, 1], [2, -1], [-2, 1], [-2, -1],
                   [1, 2], [-1, 2], [1, -2], [-1, -2]]
+    @move_tree = BinarySearchTree.new
   end
 
   def on_board?(pos)
     result = true
-    pos.each { |coord| result = false unless (0..7).include?(coord) }
+    pos.each { |coord| result = false unless (0..7).cover?(coord) }
     result
   end
 
-  def move_set
+  def move_set(pos = @start)
     move_set = @movements.map do |movement|
-      move = [movement, @pos].transpose.map(&:sum)
+      move = [movement, pos].transpose.map(&:sum)
       on_board?(move) ? move : nil
     end
     move_set.delete(nil)
     move_set
+  end
+
+  def find_path
+    @move_tree.build_tree(move_set)
+    # TODO: Add ability to insert on a certain node
   end
 end
